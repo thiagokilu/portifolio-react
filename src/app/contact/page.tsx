0eb0e1b41cf728dbff87b844a6acff623daa00b2
Thiago Alexandre <thiagokilu34@gmail.com>
2025-02-26 13:37:02 -0300

    add get form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -24,21 +24,24 @@ export default function Contact() {
             criativas, estou aqui para ajudar.
           </p>
         </div>
-        <form className="flex-1 flex flex-col gap-4" action="" method="post">
+        <form className="flex-1 flex flex-col gap-4" action="https://getform.io/f/akkynpra" method="POST">
           <input
             type="text"
             id="input-name"
+            name="nome"
             placeholder="Nome"
             className="p-2 border border-gray-300 rounded-md"
           />
           <input
             type="email"
             id="input-email"
+            name="e-mail"
             placeholder="E-mail"
             className="p-2 border border-gray-300 rounded-md"
           />
           <textarea
             placeholder="Mensagem"
+            name="mensagem"
             className="p-2 border border-gray-300 rounded-md h-32"
           />
           <input
