987b022a7efda5af99aa1c8f162c2f60dd392d3b
Thiago Alexandre <thiagokilu34@gmail.com>
2025-05-29 14:22:33 -0300

    update cards projects and big cards

--- components/bigCard.tsx
+++ components/bigCard.tsx
@@ -17,25 +17,23 @@ export function BigCard({
 }: CardProps) {
 	return (
 		<div
-			className="dark:bg-[#334155] group relative w-[600px] overflow-hidden rounded-2xl 
-        shadow-xl hover:shadow-2xl transition-shadow duration-300 sm:flex sm:flex-col sm:items-center sm:justify-center sm:text-center lg:w-xl"
+			className="dark:bg-[#334155] group relative w-full max-w-[600px] overflow-hidden rounded-2xl 
+        shadow-xl hover:shadow-2xl transition-shadow duration-300 flex flex-col"
 		>
 			<a href={linkProjeto} target="_blank" rel="noopener noreferrer">
-				<div className="overflow-hidden aspect-video">
+				<div className="overflow-hidden w-full h-64">
 					<img
 						src={linkImg}
 						alt={title}
-						className="object-cover w-full transform group-hover:scale-105 transition-transform duration-300"
-						width={1920}
-						height={1080}
+						className="object-cover w-full h-full transform group-hover:scale-105 transition-transform duration-300"
 					/>
 				</div>
-				<div className="p-6 flex flex-col items-start">
-					<div className="flex justify-between w-full">
-						<h3 className="text-xl font-semibold mb-2">{title}</h3>
-						<div className="flex gap-3 text-xl mb-3">{icons}</div>
+				<div className="p-6 flex flex-col gap-3">
+					<div className="flex justify-between items-center">
+						<h3 className="text-xl font-semibold">{title}</h3>
+						<div className="flex gap-3 text-xl">{icons}</div>
 					</div>
-					<p className="">{description}</p>
+					<p>{description}</p>
 				</div>
 			</a>
 		</div>
--- components/card-group.tsx
+++ components/card-group.tsx
@@ -1,122 +1,120 @@
 import {
-	FaHtml5,
-	FaCss3Alt,
-	FaJsSquare,
-	FaNodeJs,
-	FaReact,
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+  FaNodeJs,
+  FaReact,
 } from "react-icons/fa";
 import { Card } from "./card";
 import { BigCard } from "./bigCard";
 import { useState, useEffect } from "react";
 
 interface Project {
-	id: string;
-	name: string;
-	img: string;
-	framework: string;
-	description: string;
-	link: string;
+  id: string;
+  name: string;
+  img: string;
+  framework: string;
+  description: string;
+  link: string;
 }
 
 export function CardGroup() {
-	const [projetos, setProjetos] = useState<Project[]>([]);
-	const [loading, setLoading] = useState(false);
+  const [projetos, setProjetos] = useState<Project[]>([]);
+  const [loading, setLoading] = useState(false);
 
-	const icons = (
-		<>
-			<FaHtml5 />
-			<FaCss3Alt />
-			<FaJsSquare />
-		</>
-	);
+  const icons = (
+    <>
+      <FaHtml5 />
+      <FaCss3Alt />
+      <FaJsSquare />
+    </>
+  );
 
-	const reactIcons = (
-		<>
-			<FaReact />
-		</>
-	);
+  const reactIcons = (
+    <>
+      <FaReact />
+    </>
+  );
 
-	useEffect(() => {
-		async function getLocalProjects() {
-			setLoading(true);
-			try {
-				const res = await fetch("/assets/files/projetos.json");
-				if (!res.ok) {
-					throw new Error(`Erro ao carregar projetos.json: ${res.statusText}`);
-				}
-				const data: Project[] = await res.json();
-				setProjetos(data);
-			} catch (error) {
-				console.error(
-					"Erro ao buscar os projetos locais:",
-					error instanceof Error ? error.message : "Unknown error",
-				);
-			} finally {
-				setLoading(false);
-			}
-		}
-		getLocalProjects();
-	}, []);
+  useEffect(() => {
+    async function getLocalProjects() {
+      setLoading(true);
+      try {
+        const res = await fetch("/assets/files/projetos.json");
+        if (!res.ok) {
+          throw new Error(`Erro ao carregar projetos.json: ${res.statusText}`);
+        }
+        const data: Project[] = await res.json();
+        setProjetos(data);
+      } catch (error) {
+        console.error(
+          "Erro ao buscar os projetos locais:",
+          error instanceof Error ? error.message : "Unknown error"
+        );
+      } finally {
+        setLoading(false);
+      }
+    }
+    getLocalProjects();
+  }, []);
 
-	return (
-		<div>
-			<div className="flex flex-wrap w-full gap-5 mb-10">
-				<BigCard
-					linkProjeto="https://soundify-five.vercel.app/"
-					linkImg="https://res.cloudinary.com/dbwz36bcf/image/upload/v1741477590/Soundify-16-9_p9hk7c.png"
-					title="Soundify"
-					description="Plataforma de streaming musical com integração de API e player customizado."
-					icons={
-						<div className="flex gap-3 text-xl mb-3">
-							<FaHtml5 />
-							<FaCss3Alt />
-							<FaJsSquare />
-							<FaNodeJs />
-						</div>
-					}
-				/>
+  return (
+    <div>
+      <div className="flex flex-col lg:flex-row gap-5 mb-10">
+        <BigCard
+          linkProjeto="https://soundify-five.vercel.app/"
+          linkImg="https://res.cloudinary.com/dbwz36bcf/image/upload/v1741477590/Soundify-16-9_p9hk7c.png"
+          title="Soundify"
+          description="Plataforma de streaming musical com integração de API e player customizado."
+          icons={
+            <div className="flex gap-3 text-xl mb-3">
+              <FaHtml5 />
+              <FaCss3Alt />
+              <FaJsSquare />
+              <FaNodeJs />
+            </div>
+          }
+        />
 
-				<BigCard
-					linkProjeto="https://nature-ecomerce.vercel.app/"
-					linkImg="https://res.cloudinary.com/dbwz36bcf/image/upload/v1741477591/nature-ecomerce-16-9_q6tmvd.png"
-					title="Nature E-commerce"
-					description="Loja virtual focada em produtos sustentáveis e ecológicos."
-					icons={
-						<div className="flex gap-3 text-xl mb-3">
-							<FaHtml5 />
-							<FaCss3Alt />
-							<FaJsSquare />
-						</div>
-					}
-				/>
-			</div>
+        <BigCard
+          linkProjeto="https://astroxzdesigner.com.br/"
+          linkImg="https://res.cloudinary.com/ds4ptms7d/image/upload/v1748537447/Captura_de_tela_de_2025-05-29_13-49-36_fgsone.png"
+          title="Artroxz"
+          description="Portfólio criativo com foco em design e desenvolvimento visual."
+          icons={
+            <div className="flex gap-3 text-xl mb-3">
+              <FaReact />
+            </div>
+          }
+        />
+      </div>
 
-			<h2 className="text-4xl font-bold text-center mb-16">Outros projetos</h2>
+      <h2 className="text-4xl font-bold text-center mb-16">Outros projetos</h2>
 
-			{loading && (
-				<div className="w-full flex justify-center items-center mb-10">
-					<div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-				</div>
-			)}
+      {loading && (
+        <div className="w-full flex justify-center items-center mb-10">
+          <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        </div>
+      )}
 
-			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
-				{projetos.map((proj) => {
-					return (
-						<Card
-							key={proj.id}
-							linkProjeto={proj.link}
-							linkImg={proj.img}
-							title={proj.name}
-							description={proj.description}
-							icons={
-								<div className="flex gap-3 text-xl mb-3">
-									{proj.framework === "padrao" ? icons : reactIcons}
-								</div>
-							}
-						/>
-					);
-				})}
-			</div>
-		</div>
-	);
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
+        {projetos.map((proj) => {
+          return (
+            <Card
+              key={proj.id}
+              linkProjeto={proj.link}
+              linkImg={proj.img}
+              title={proj.name}
+              description={proj.description}
+              icons={
+                <div className="flex gap-3 text-xl mb-3">
+                  {proj.framework === "padrao" ? icons : reactIcons}
+                </div>
+              }
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
