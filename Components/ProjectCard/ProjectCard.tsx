58822f78ffa10261da43776494de73ec8a3e2d65
Thiago Alexandre <thiagokilu34@gmail.com>
2025-12-10 10:52:54 -0300

    add link in project

--- Components/ProjectCard/ProjectCard.tsx
+++ Components/ProjectCard/ProjectCard.tsx
@@ -46,51 +46,53 @@ export const techIcons: Record<string, JSX.Element> = {
 
 export default function ProjectCard({ project }: any) {
   return (
-    <div
-      className="group bg-white/5 backdrop-blur-md border border-white/10 
+    <a href={project.link} target="_blank" rel="noopener noreferrer">
+      <div
+        className="group bg-white/5 backdrop-blur-md border border-white/10 
                  hover:border-purple-primary/60 hover:shadow-purple-primary/30 
                  shadow-[0_0_20px_-5px_rgba(0,0,0,0.4)]
                  transition-all rounded-2xl p-5 flex flex-col gap-5 w-full max-w-sm"
-    >
-      {/* Imagem */}
-      <div className="w-full aspect-video overflow-hidden rounded-xl">
-        <img
-          src={project.img}
-          alt="Projeto"
-          className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
-        />
-      </div>
-
-      {/* Conteúdo */}
-      <div className="flex flex-col gap-3">
-        {/* Título + Tecnologias */}
-        <div className="flex flex-col gap-1">
-          <h3 className="text-white text-xl font-semibold tracking-wide">
-            {project.title}
-          </h3>
-
-          <div className="flex flex-row flex-wrap gap-2">
-            {project.techs.map((tech: string) => (
-              <span
-                key={tech}
-                className="px-3 py-1 flex items-center gap-1 text-sm 
-               bg-purple-primary/20 text-purple-primary rounded-lg 
-               border border-purple-primary/30"
-              >
-                {techIcons[tech] && (
-                  <span className="text-lg">{techIcons[tech]}</span>
-                )}
-                {tech}
-              </span>
-            ))}
-          </div>
+      >
+        {/* Imagem */}
+        <div className="w-full aspect-video overflow-hidden rounded-xl">
+          <img
+            src={project.img}
+            alt="Projeto"
+            className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+          />
         </div>
 
-        {/* Descrição */}
-        <p className="text-gray-300 text-sm leading-relaxed">
-          {project.description}
-        </p>
+        {/* Conteúdo */}
+        <div className="flex flex-col gap-3">
+          {/* Título + Tecnologias */}
+          <div className="flex flex-col gap-1">
+            <h3 className="text-white text-xl font-semibold tracking-wide">
+              {project.title}
+            </h3>
+
+            <div className="flex flex-row flex-wrap gap-2">
+              {project.techs.map((tech: string) => (
+                <span
+                  key={tech}
+                  className="px-3 py-1 flex items-center gap-1 text-sm 
+               bg-purple-primary/20 text-purple-primary rounded-lg 
+               border border-purple-primary/30"
+                >
+                  {techIcons[tech] && (
+                    <span className="text-lg">{techIcons[tech]}</span>
+                  )}
+                  {tech}
+                </span>
+              ))}
+            </div>
+          </div>
+
+          {/* Descrição */}
+          <p className="text-gray-300 text-sm leading-relaxed">
+            {project.description}
+          </p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
