eaf18ce301cb313e5252cb7db4adc411b1164f67
Thiago Alexandre <thiagokilu34@gmail.com>
2026-01-15 14:30:06 -0300

    solve build bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,6 @@
-import About from "@/Components/About/About";
-import Contact from "@/Components/Contact/Contact";
-import ContractedServices from "@/Components/ContractedServices/ContractedServices";
-import Footer from "@/Components/Footer/Footer";
-import Hero from "@/Components/Hero/Hero";
-import Projects from "@/Components/Projects/Projects";
-import Skills from "@/Components/Skills/Skills";
-import React from "react";
+import { redirect } from "next/navigation";
 
-export default function home() {
-  return (
-    <div className="flex flex-col gap-44">
-      <Hero />
-      <Skills />
-      <About />
-      <Projects />
-      <ContractedServices />
-      <Contact />
-      <Footer />
-    </div>
-  );
+export default function RootPage() {
+  redirect("/pt");
 }
+
